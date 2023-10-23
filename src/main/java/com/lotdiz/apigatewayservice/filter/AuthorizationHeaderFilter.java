c2057a8138ed842a91e4b07d889e28cd0d64b829
leewooyup <zjadlspun1114@naver.com>
2023-10-23 16:09:32 +0900

    feat: setting application.yml & add AuthorizationHeaderFilter

--- src/main/java/com/lotdiz/apigatewayservice/filter/AuthorizationHeaderFilter.java
+++ src/main/java/com/lotdiz/apigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -1,9 +1,11 @@
 package com.lotdiz.apigatewayservice.filter;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import javax.crypto.SecretKey;
 import lombok.Data;
 import org.slf4j.Logger;
@@ -16,8 +18,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.PathContainer;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-
-
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.util.pattern.PathPattern;
@@ -43,54 +43,73 @@ public class AuthorizationHeaderFilter
 
       ServerHttpRequest request = exchange.getRequest();
 
-      if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-        return onError(exchange, "no authorization header", HttpStatus.UNAUTHORIZED);
-      }
-
-      String jwtHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
-      String jwt = jwtHeader.replace("Bearer ", "");
-
-      String secret = env.getProperty("jwt.secret");
-      byte[] keyBytes = Decoders.BASE64.decode(secret);
-      SecretKey key = Keys.hmacShaKeyFor(keyBytes);
-      Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
-      String username = claims.get("username", String.class);
-
-      if (username == null) {
-        return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
-      }
-      // Token 정보를 까서 memberId를 header에 저장
-      String memberId = claims.get("memberId", String.class);
-      logger.info("memberId: " + memberId);
-
-      HttpHeaders originHeaders = request.getHeaders();
-      // 새로운 HttpHeaders 객체 생성
-      HttpHeaders newHeaders = new HttpHeaders();
-
-      newHeaders.set("memberId", memberId);
-      newHeaders.addAll(originHeaders);
-
-      ServerHttpRequest modifiedRequest = request.mutate().headers((h) -> h.addAll(newHeaders)).build();
-
-      ServerWebExchange modifiedExchange = exchange.mutate().request(modifiedRequest).build();
-
-      // if path = /admin-service/** -> Role 확인 (admin인지)
       PathPatternParser pathPatternParser = new PathPatternParser();
-      PathPattern pathPattern = pathPatternParser.parse("/admin-service/**");
+      PathPattern pathPattern = null;
       PathContainer pathContainer =
           PathContainer.parsePath(request.getPath().pathWithinApplication().value());
-      if (pathPattern.matches(pathContainer)) {
-        String auth = claims.get("auth", String.class);
-        if (!auth.equals("ADMIN")) { // admin 이 아니면 403
-          return onError(exchange, "Not Admin", HttpStatus.FORBIDDEN);
+      logger.info("path: " + request.getPath().pathWithinApplication().value());
+
+      String username = null;
+      if (request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) { // 로그인 중
+        String jwtHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+        String jwt = jwtHeader.replace("Bearer ", "");
+
+        String secret = env.getProperty("jwt.secret");
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        SecretKey key = Keys.hmacShaKeyFor(keyBytes);
+
+        Claims claims = checkValid(jwt, key); // 토큰 유효성 검사
+
+        username = claims.get("username", String.class);
+
+        if (username == null) {
+          return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
+        }
+
+        // Token 정보를 까서 memberId를 header에 저장
+        String memberId = claims.get("memberId", String.class);
+        logger.info("memberId: " + memberId);
+        exchange.getRequest().mutate().header("memberId", memberId).build();
+
+        // if path = /admin-service/** -> Role 확인 (admin인지)
+        pathPattern = pathPatternParser.parse("/api/admin/**");
+        if (pathPattern.matches(pathContainer)) {
+          String auth = claims.get("auth", String.class);
+          if (!auth.equals("ADMIN")) { // admin 이 아니면 403
+            return onError(exchange, "Not Admin", HttpStatus.FORBIDDEN);
+          }
+        }
+      } else { // 비로그인 중
+        // project는 비로그인이어도 갈 수 있는 페이지가 존재.
+        pathPattern = pathPatternParser.parse("/api/projects/**");
+        String curPath = request.getPath().toString();
+        logger.info("curPath: " + curPath);
+        if (pathPattern.matches(pathContainer)) { // if, access MSA is project-service
+          if (!curPath.contains("support-signature") && !curPath.contains("makers")) {
+            username = "NON_MEMBER";
+          } else {
+            return onError(exchange, "no authorization header", HttpStatus.UNAUTHORIZED);
+          }
+        } else {
+          return onError(exchange, "no authorization header", HttpStatus.UNAUTHORIZED);
         }
       }
-
       logger.info("AuthorizationHeader Filter End");
-      return chain.filter(modifiedExchange);
+      return chain.filter(exchange);
     });
   }
 
+  private Claims checkValid(String jwt, SecretKey key) {
+    try {
+      return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+    } catch (IllegalArgumentException e) {
+      throw new RuntimeException("잘못된 JWT 토큰입니다.");
+    } catch (ExpiredJwtException e) {
+      throw new RuntimeException("만료된 JWT 토큰입니다.");
+    } catch (SignatureException e) {
+      throw new RuntimeException("잘못된 JWT 서명입니다.");
+    }
+  }
 
   private Mono<Void> onError(ServerWebExchange exchange, String error, HttpStatus httpStatus) {
     ServerHttpResponse response = exchange.getResponse();
